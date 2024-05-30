13de1744afed4f1b98a500762cdb0e0aa49272a5
dekhs <sukrut.dekhane@pega.com>
2024-05-30 21:48:35 +0530

    Added pagination buttons and fetched data from api

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-
+let productsArray = [];
+fetch("https://dummyjson.com/products")
+  .then((res) => res.json())
+  .then((data) => {
+    productsArray = data.products;
+    console.log(productsArray);
+    console.log(productsArray.length);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
